c44855b11266716d481f87f963281559a6417071
Bluperman949 <christiandoolittle@hotmail.com>
2026-03-24 23:12:43 -0700

    save api key to private storage

--- app/src/main/java/bluper/b9final/ApiKeys.kt
+++ app/src/main/java/bluper/b9final/ApiKeys.kt
@@ -4,13 +4,19 @@ import android.app.AlertDialog
 import android.content.Context
 import android.text.InputType
 import android.widget.EditText
-
-// TODO: save to disk
+import java.io.File
 
 var apiKey: String = "NO_API_KEY"
   private set
 
 fun acquireApiKey(ctx: Context) {
+  val file = File(ctx.noBackupFilesDir, "steam")
+  // refresh key if it's been more than 24 hours
+  if (file.exists() && System.currentTimeMillis() - file.lastModified() < 86400000) {
+    apiKey = file.readText()
+    return
+  }
+
   val input = EditText(ctx)
   input.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
 
@@ -18,6 +24,10 @@ fun acquireApiKey(ctx: Context) {
     .setView(input)
     .setTitle(R.string.ask_api_key)
     .setMessage(R.string.message_api_key)
-    .setPositiveButton("OK") { _, _ -> apiKey = input.text.toString() }
+    .setPositiveButton("OK") { _, _ ->
+      apiKey = input.text.toString()
+      file.createNewFile()
+      file.writeText(apiKey)
+    }
     .show()
 }
